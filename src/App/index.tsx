6a5c76e636a036123a4b1844e1c3671f7d1d699f
zhangzhichao@shangqikj.com <stanleyxu@stanleyxudeMacBook-Pro.local>
2021-09-16 18:22:07 +0800

    xx

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -3,13 +3,14 @@ import Style from './style.less'
 
 function App() {
   let a = ['a', 'b']
+  c
   return (
     <div className={Style.test}>
       <h1>123123阿斯顿发生</h1>
       <h2>Welcome to React App</h2>
       <h3>Date: {new Date().toDateString()}</h3>
       <span className={Style.iconfont}>&#xe871;</span>
-      <div>{a}fff</div>
+      <div>{a}</div>
     </div>
   )
 }
